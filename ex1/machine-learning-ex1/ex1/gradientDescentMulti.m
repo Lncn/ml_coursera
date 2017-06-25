511f0b17882a5f791d87350e5e3a23fe8ad93059
Lincoln Simmons <lincolnsimmons@gmail.com>
2017-06-25 12:23:05 -0500

    Assignment 1 answers
    - Data plotting
    - Cost function for linear regression
    - Gradient descent for linear regression
    - Feature normalization
    - Normal equation implementation
    - Cost function for multivariate linear regression
    - Gradient descent for multivaritate linear regression

--- ex1/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ ex1/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,16 +16,15 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    
+    derivative = theta;
+    h = (X * theta - y);
 
+    for ii=1:length(derivative)
+        derivative(ii) = sum(h .* X(:,ii));
+    end
 
-
-
-
-
-
-
-
-
+    theta = theta - (alpha / m) * derivative;
 
     % ============================================================
 
